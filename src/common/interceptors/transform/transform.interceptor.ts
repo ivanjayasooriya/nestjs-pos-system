5d7ca72a87b1d1fd272adcfc435b25feb9cb0bca
Ivan Adithya <ivanjayasooriya03@gmail.com>
2026-09-09 13:25:53 +0530

    feat: enhance TransformInterceptor to include detailed response structure

--- src/common/interceptors/transform/transform.interceptor.ts
+++ src/common/interceptors/transform/transform.interceptor.ts
@@ -1,9 +1,39 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+
+export interface Response<T> {
+  success: boolean;
+  statusCode: number;
+  message: string;
+  data: T | null;
+  timestamp: string;
+}
 
 @Injectable()
-export class TransformInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next.handle();
+export class TransformInterceptor<T> implements NestInterceptor<
+  T,
+  Response<T>
+> {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<Response<T>> {
+    const response = context.switchToHttp().getResponse();
+    const statusCode = response.statusCode;
+
+    return next.handle().pipe(
+      map((data: T) => ({
+        success: true,
+        statusCode,
+        message: 'Request successful',
+        data: data ?? null,
+        timestamp: new Date().toISOString(),
+      })),
+    );
   }
 }
