8a42465445d7f404a68737f0abb268d55cc6dd9b
Ivan Adithya <ivanjayasooriya03@gmail.com>
2026-09-10 11:41:28 +0530

    Refactor code structure for improved readability and maintainability

--- src/common/middleware/logging/logging.middleware.ts
+++ src/common/middleware/logging/logging.middleware.ts
@@ -4,7 +4,7 @@ import { NextFunction, Request, Response } from 'express';
 @Injectable()
 export class LoggingMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    console.log(`Request: ${req.method} ${req.url}`);
+    console.log(`Request: ${req.method} ${req.originalUrl}`);
     next();
   }
 }
